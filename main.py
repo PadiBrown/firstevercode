e3566a90a76fa2a16207a20c4a6c2591a9df1a3f
padib <padi.brown@outlook.com>
2021-03-03 19:41:30 +0100

    my first ever commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print('hello world')
+print('hello world 2\n')
+print(6+7)
+print(6+7-11+56)
+print((2+2)*5)
+print("the result of 6X7 is", 6*7, "this was pretty cool")
+print(7/0)
+print('this is the end part')
